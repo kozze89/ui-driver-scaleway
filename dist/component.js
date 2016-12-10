60455a3580415605edf050e1851d262051fa6e0e
Konstantinos Vaggelakos <kvaggelakos@climate.com>
2016-12-10 16:07:50 -0500

    Add dist folder

---
+++ dist/component.js
@@ -0,0 +1,58 @@
+/* v----- Do not change anything between here
+ *       (the DRIVERNAME placeholder will be automatically replaced during build) */
+define('ui/components/machine/driver-skel/component', ['exports', 'ember', 'ui/mixins/driver'], function (exports, _ember, _uiMixinsDriver) {
+
+  exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
+    driverName: 'skel',
+/* ^--- And here */
+
+    // Write your component here, starting with setting 'model' to a machine with your config populated
+    bootstrap: function() {
+      let config = this.get('store').createRecord({
+        type        : 'skelConfig',
+        size        : 512,
+      });
+
+      this.set('model', this.get('store').createRecord({
+        type: 'machine',
+        'skelConfig': config,
+      }));
+    },
+
+    // Add custom validation beyond what can be done from the config API schema
+    validate() {
+      // Get generic API validation errors
+      this._super();
+      var errors = this.get('errors')||[];
+
+      // Add more specific errors
+
+      // Check something and add an error entry if it fails:
+      if ( parseInt(this.get('model.skelConfig.size'),10) < 1024 )
+      {
+        errors.push('Size must be at least 1024 MB');
+      }
+
+      // Set the array of errors for display,
+      // and return true if saving should continue.
+      if ( errors.get('length') )
+      {
+        this.set('errors', errors);
+        return false;
+      }
+      else
+      {
+        this.set('errors', null);
+        return true;
+      }
+    },
+
+    // Any computed properties or custom logic can go here
+  });
+});
+;
+define("ui/components/machine/driver-skel/template",["exports","ember","ui/mixins/driver"],function(exports,_ember,_uiMixinsDriver){
+
+exports["default"] = Ember.HTMLBars.template({"id":"XGk/Fin+","block":"{\"statements\":[[\"open-element\",\"section\",[]],[\"static-attr\",\"class\",\"horizontal-form\"],[\"flush-element\"],[\"text\",\"\\n  \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"container-fluid\"],[\"flush-element\"],[\"text\",\"\\n\"],[\"text\",\"    \"],[\"partial\",\"host/add-common\"],[\"text\",\"\\n\\n    \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"over-hr r-mt20 r-mb20\"],[\"flush-element\"],[\"text\",\"\\n      \"],[\"open-element\",\"span\",[]],[\"flush-element\"],[\"text\",\"My Skeleton Driver\"],[\"close-element\"],[\"text\",\"\\n    \"],[\"close-element\"],[\"text\",\"\\n\\n\"],[\"text\",\"    \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"row\"],[\"flush-element\"],[\"text\",\"\\n      \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"col-sm-12 col-md-2 form-label\"],[\"flush-element\"],[\"text\",\"\\n        \"],[\"open-element\",\"label\",[]],[\"static-attr\",\"class\",\"form-control-static\"],[\"flush-element\"],[\"text\",\"Size\"],[\"close-element\"],[\"text\",\"\\n      \"],[\"close-element\"],[\"text\",\"\\n      \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"col-sm-12 col-md-4\"],[\"flush-element\"],[\"text\",\"\\n        \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"input-group\"],[\"flush-element\"],[\"text\",\"\\n          \"],[\"append\",[\"helper\",[\"input\"],null,[[\"type\",\"class\",\"value\"],[\"text\",\"form-control\",[\"get\",[\"model\",\"skelConfig\",\"size\"]]]]],false],[\"text\",\"\\n          \"],[\"open-element\",\"span\",[]],[\"static-attr\",\"class\",\"input-group-addon\"],[\"flush-element\"],[\"text\",\"MB\"],[\"close-element\"],[\"text\",\"\\n        \"],[\"close-element\"],[\"text\",\"\\n      \"],[\"close-element\"],[\"text\",\"\\n    \"],[\"close-element\"],[\"text\",\"\\n\\n\"],[\"text\",\"    \"],[\"partial\",\"host/add-options\"],[\"text\",\"\\n  \"],[\"close-element\"],[\"text\",\"\\n\\n\"],[\"text\",\"  \"],[\"append\",[\"helper\",[\"top-errors\"],null,[[\"errors\"],[[\"get\",[\"errors\"]]]]],false],[\"text\",\"\\n\\n\"],[\"text\",\"  \"],[\"append\",[\"helper\",[\"save-cancel\"],null,[[\"save\",\"cancel\"],[\"save\",\"cancel\"]]],false],[\"text\",\"\\n\"],[\"close-element\"],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":true}","meta":{}});;
+
+});
